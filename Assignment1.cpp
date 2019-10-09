77f3955593ed95a8eb148d8eb4318db21423baeb
moccbc <yutanak2010@gmail.com>
2019-10-09 13:34:24 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -12,22 +12,16 @@ int main(){
   string userPoly;
   // The vecotr that is going to store each individual term
   vector<string> polyTerms;
-
-  vector<string>::iterator it1;
   // Gets the user input
   getline(cin, userPoly);
   // Test for isValid
   cout << isValid(userPoly, polyTerms) << endl;
 
-/*
+  // Print the contents of polyTerms
   for (int i = 0; i < polyTerms.size(); i++){
     cout << polyTerms.at(i) << endl;
   }
-*/
 
-  for(it1 = polyTerms.begin(); it1 != polyTerms.end(); it1++) {
-    cout << *it1 << endl;
-  }
   return 0;
 }
 
@@ -43,27 +37,23 @@ bool isValid(string userPolynomial, vector<string> &polyTerms){
     }
   }
 
-  // If is does not include '-', '(', ')'
+  // If is does not include '-', '(', ')' split the expression into each term
   for (int j = 0; j < userPolynomial.length(); j++){
-    cout << "Runs up to here 1" << endl;
     positionOfPlus = userPolynomial.find("+", j);
     // If there are no more '+' symbols to look for
     if (positionOfPlus == string::npos){
       positionOfPlus = userPolynomial.length();
     }
-    cout << "Runs up to here 2" << endl;
     // Store 1 term
     for (int i = j; i < positionOfPlus; i++){
-      cout << "Runs up to here 3" << endl;
-      term[i] = userPolynomial[i];
-      cout << term[i] << endl;
+      term += userPolynomial[i];
     }
-    cout << "Runs up to here 4" << endl;
     // Store the term into the vector
     polyTerms.push_back(term);
 
     j = positionOfPlus;
-
+    term.clear();
   }
+
   return false;
 }
