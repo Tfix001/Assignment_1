f6feb5b291601425bfb76a964e6de02fbde8f683
Tfix001 <56132452+Tfix001@users.noreply.github.com>
2019-10-09 08:28:17 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World" << endl;
+  //Check invalidity i.e. Check for negation and/or parathesis --
+  //Check validity i.e. Check for summation and/or multiplication --
+  //Second check for invalidity i.e. n^3n && n^4.2 --
+  string userPoly;
+  getline(cin, userPoly);
+  for(unsigned i = 0; i < userPoly.size(); i++){
+    //Check string for top three
+    //Can also be used to look for highest degree
+  }
+  
+  //Check for highest degree (aka Big-O notation) --
+  // If the length of the input expression is m chars, what is the big-Oh complexity of your program with respect to m? --
+  
+  //What if we require that there is only one term for each degree? --
+    //That is, (ii) above is invalid because it has two terms for degree 1 (n^1).
   return 0;
 }
