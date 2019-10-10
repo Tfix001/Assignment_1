f5bb74786d263d1533019c193236fdae084f6505
Tfix001 <56132452+Tfix001@users.noreply.github.com>
2019-10-10 00:00:59 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-int degree = 0;
-string string;
-string newString;
-
-int main () {
-  for(int i = 0; i< string.length(); i++){
-    i = string.find("n", i){
-    if(string[i++]=='^'){
-      for(int j = i+2;(string[j]!='+') || (j!= string.length); j++) {
-        newString+=string[j];
+string findDegrees(string userPoly){
+  string newString;
+  for(int i = 0; i< userPoly.length(); i++){
+    i = userPoly.find("n", i){
+    if(userPoly[i++]=='^'){
+      for(int j = i+2;(userPoly[j]!='+') || (j!= userPoly.length); j++) {
+        newString+=userPoly[j];
       }
       newString+=" ";
     }
     else {
       newString +='1';
       newString +=" ";
+    }
   }
 }
-}
-if(newString.isEmpty()) {
-  degree = 0;
-}
+  if(newString.isEmpty()) {
+    newString = "0";
+  }
 }
