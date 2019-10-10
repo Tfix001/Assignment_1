c88297b03cbf4cc0f726c38afd8ea4656f92e3a8
moccbc <yutanak2010@gmail.com>
2019-10-09 21:09:02 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,9 +1,14 @@
 /*
 * Assignment 1
 * Written by: Taylor Fix, Jacob Liu, Yuta Nakamura
-*  
-*  
-*  
+* Usage: To check whether a given expression is a polynomial or not
+* Restrictions:
+*   - All multiplication between numbers and variables are assumed to be resolved before being entered
+*     eg) Instead of 3*2, assume input 6. Likewise, instead of n*n*n, assume input n^3
+*   - Variables within the expression must be consistant within the expression
+*   - All variables are assumed to be denoted by 'n'
+*
+*
 */
 
 #include <iostream>
@@ -12,13 +17,60 @@
 
 using namespace std;
 
+/*----------- Function Prototypes -----------*/
+// Function to remove whitespace
+string removeWhiteSpace(string input);
+// Function to see if the expression is a valid polynomial
+bool isPolynomial(string input);
+
+
+/*----------- Main Function -----------*/
 int main() {
   // This is the user input
   string userPoly;
   // Gets the input from the user
   getline(cin, userPoly);
+  // Remove the whitespace from the string
+  userPoly = removeWhiteSpace(userPoly);
+
+  if (isPolynomial(userPoly))
+  {
+    cout << "This is a valid polynomial." << endl;
+  }
+  else
+  {
+    cout << "This is not a valid polynomial." << endl;
+  }
+
+
+}
+
+/*---------- Function Definition ----------*/
+// Removes the white space within the string
+string removeWhiteSpace(string input)
+{
+  // New string without the whitespace.
+  string noWhitespace;
+  // The new position that the int is going to take in the temp string
+  int newPosition = 0;
+  // For the length of the string
+  for (int i = 0; i < input.length(); i++)
+  {
+    // If the char in the string is not a whitespace
+    if (input[i] != ' ')
+    {
+      // Copy that into the new string
+      noWhitespace += input[i];
+    }
+  }
+  return noWhitespace;
+}
+
+// Returns if the expression is a polynomial
+bool isPolynomial(string userPoly){
   // This is assuming that the polynomial starts with 'n' or a number
   char state = 'A';
+  bool isPolynomial = 1;
 
   // For the length of the entire polynomial
   for(int i = 0; i < userPoly.length() + 1; i++) {
@@ -66,7 +118,7 @@ int main() {
         }
         break;
 
-      case 'D': 
+      case 'D':
         if(isdigit(userPoly[i])){  // If the char is a number
           state = 'E';
         }
@@ -75,7 +127,7 @@ int main() {
         }
         break;
 
-      case 'E': 
+      case 'E':
         if(isdigit(userPoly[i])){  // If the char is a number
           state = 'E';
         }
@@ -134,17 +186,17 @@ int main() {
 
       // The expression is deemed not to be a polynomial when it does not fit the above criterias
       default:
-        cout << "Invalid Polynomial" << endl;
-        return false;
+        isPolynomial = 0;
         break;
      }
    }
-   
+
   // If the last character in the string is '.', '+', or '*' then return that it is not a polynomial
    if(userPoly.size() > 0){
-        if(!isdigit(userPoly[userPoly.length() - 1]) || userPoly[userPoly.length() - 1] != 'n'){
-            cout << "Invalid Polynomial" << endl;
-            return false;
+        if(!isdigit(userPoly[userPoly.length() - 1]) && userPoly[userPoly.length() - 1] != 'n'){
+            isPolynomial = 0;
         }
    }
+
+   return isPolynomial;
 }
