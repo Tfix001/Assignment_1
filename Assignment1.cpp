0fddad9d913aef7a65af422aef9310a7d8d77bca
Tfix001 <56132452+Tfix001@users.noreply.github.com>
2019-10-10 22:51:14 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -21,43 +21,7 @@ Examples:
 18 = 3 * 3 * 2 * 1 OR 6 * 3 * 1
 20 = 10 * 2 * 1
 DOES NOT WORK (unless 1 is in the Array): 37 = 37 * 1
-
-Using pseudocode method shown below (Currently)
-
-first target = 37
-array = 16 2 1 37
-after first findSubset call
-target = 37
-array = 16 2 0 37
-subsetarray = 1
-after second findSubset call
-target = 37
-array = 16 2 0 37
-subsetarray = 1 37
-Should return the subsetArray? Should return somehow.
---Meaning that the subset gets back to the main function. 
-  Either by the output of the function, or since it was a reference in the function?
-
-
-
-*  Taylor's Theory: 
-*  (Possibly recurrsive program?)
-*  Call a function with an array, a target number, and a subsetArray? (Not sure how to get the subset otherwise)
-*  --Either check if the array is empty here or in the main function
-*    If array at index is 0 -> skip it
-*    If we can find a number such that target number % array at index is 0   (% 1 gives 0 as output too)
-*      -> newNumber becomes the array at index
-*      -> subsetArray gets the newNumber
-*      -> If subsetArray product does not equal target number ------ Helper function?
-*        -> target number becomes target number / newNumber
-*        -> array at index becomes 0
-*        -> call the function with the new array, and target number (Should be simpler to check through?)
-*      -> Else the product does equal target number and we can return the subsetArray.
-*    Else the target number does not have a subset of array ints that will produce it as a product
-*  End Call
 */
-
-//If my thinking is correct, we should really only have O(n) here. Since I don't think there should be a nested loop
 
 //Array[size] = {what's inside the array}
 
