cdd443305f0ce5eb8bc7afa274287d083cc6a5d5
Tfix001 <56132452+Tfix001@users.noreply.github.com>
2019-10-11 13:19:26 -0700

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,1 +1,27 @@
+/*Names: Jacob Liu, Taylor Fix, Yuta Nakamara
 
+Assignment 2: 
+
+If the elements of a list are sorted, is an array-based or a linked-list-based implementation of the list 
+  more efficient for binary search? Explain. 
+    Taylor's Thought: Array-based, since you have the actual size to look at. A linked-list will not be continuous block to look at efficiently
+
+Write C++ code to implement an integer queue class using linked-list, where the nodes are
+  stored sorted by ascending value of the integer they store. We call this a priority queue.
+  Specifically, implement enqueue and dequeue methods.
+
+  What is the average asymptotic cost per call to enqueue and to dequeue? Taylor's Thought: O(1) (This was discussed in class)
+
+  What if for each node, in addition to a pointer to the next node, you add a pointer to the 10th
+    next node. Modify your code to take advantage of this.
+
+  Can the modification in (c) improve the cost (not asymptotic but just execution time) of
+    enqueue? Does it improve the asymptotic cost?
+
+  Is there any disadvantage that modification (c) incurs?
+
+Write a C++ class that implement two stacks using a single C++ array. That is, it should have functions
+  popFirst(…), popSecond(…), pushFirst(…), pushSecond(…),… When out of space, double the size of the
+  array (similarly to what vector is doing).
+
+*/
